d1cf9be4f9d7c9a5345e9568e6da0ee4839e05c8
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-02-14 20:58:58 +0200

    u

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -11,7 +11,7 @@ export default function Reservation() {
             <FaBasketShopping />
           </>
         }
-        src="/icons&images/restaurant.jpg"
+        src="/icons&images/restaurant.svg"
       />
     </>
   );
