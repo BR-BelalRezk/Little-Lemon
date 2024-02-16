ec137040ce6899ed789ca7bc9b18e99d2aa16efe
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-02-17 00:11:02 +0200

    u

--- src/components/content/MenuFilter.tsx
+++ src/components/content/MenuFilter.tsx
@@ -1,4 +1,4 @@
-import { Link, SetURLSearchParams } from "react-router-dom";
+import { SetURLSearchParams } from "react-router-dom";
 import { menu } from "../../data/data";
 import YellowButton from "../shared/YellowButton";
 
@@ -9,13 +9,7 @@ export default function MenuFilter({
 }) {
   return (
     <div className=" my-5 sm:mt-20">
-      <div className="flex flex-col items-center justify-center gap-5 rounded-md bg-primary_1 px-3  py-5">
-        <Link
-          to={"."}
-          className="rounded-full bg-highlight_1 px-5 py-1 text-2xl font-bold text-highlight_2 duration-500 hover:scale-110 sm:px-10 sm:text-3xl"
-        >
-          Clear Filter
-        </Link>
+      <div className="rounded-md bg-primary_1 px-3  py-5">
         <div className="grid w-full grid-cols-3 gap-5  text-2xl text-primary_2 sm:grid sm:grid-cols-3 sm:gap-5 sm:text-highlight_2">
           {menu.map((item) => (
             <YellowButton
